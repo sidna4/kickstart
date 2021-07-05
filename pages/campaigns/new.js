f92fd893d1ddce6216b5102906398558040e2cb7
Sidney Antommarchi <sidna4@yahoo.com>
2021-07-05 11:58:00 -0400

    193

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -3,15 +3,19 @@ import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
+import { Router } from '../../routes';
 
 class CampaignNew extends Component {
     state = {
         minimumContribution: '',
-        errorMessage: ''
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
+
+        this.setState({ loading: true, errorMessage: '' });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -20,9 +24,12 @@ class CampaignNew extends Component {
                 .send({
                     from: accounts[0]
                 });
+            Router.pushRoute('/');
         } catch (err) {
             this.setState({ errorMessage: err.message });
         }
+
+        this.setState({ loading: false });
     };
 
     render() {
@@ -51,7 +58,9 @@ class CampaignNew extends Component {
                         header="Oops!"
                         content={this.state.errorMessage}
                     />
-                    <Button primary>Create!</Button>
+                    <Button loading={this.state.loading} primary>
+                        Create!
+                    </Button>
                 </Form>
             </Layout>
         );
