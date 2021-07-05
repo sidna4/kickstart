d4ca5c44cf0a04501765704eceef7a5c49047f11
Sidney Antommarchi <sidna4@yahoo.com>
2021-07-05 12:42:32 -0400

    198

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
 const routes = require('next-routes')();
 
-module.exports = routes;
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show');
 // ()
 //     .add('about')
 //     .add('blog', '/blog/:slug')
 //     .add('user', '/user/:id', 'profile')
 //     .add('/:noname/:lang(en|es)/:wow+', 'complex')
 //     .add({ name: 'beta', pattern: '/v3', page: 'v3' });
+
+module.exports = routes;
