77462db88247184f604c8407012b6a2b745f860a
Sidney Antommarchi <sidna4@yahoo.com>
2021-07-05 09:51:51 -0400

    Lecture 184

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
-import 'semantic-ui-css/semantic.min.css';
 import Layout from '../components/Layout';
 
 // CampaignIndex.getInitialProps();
